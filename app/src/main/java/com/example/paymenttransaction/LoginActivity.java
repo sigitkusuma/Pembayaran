2ece7c31a554b54747bdbfa8af63f83bb07d40dd
sigitkusuma <sigitsda71@gmail.com>
2020-12-14 15:49:12 +0700

    adding login

--- app/src/main/java/com/example/paymenttransaction/LoginActivity.java
+++ app/src/main/java/com/example/paymenttransaction/LoginActivity.java
@@ -2,13 +2,28 @@ package com.example.paymenttransaction;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signIn = findViewById(R.id.btn_signIn);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, HomeScreen.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
